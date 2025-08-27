29ac0b16b9edec6eb560af8df889c01cd78d108f
Nawaf <Alzhraniii999@gmail.com>
2025-08-27 14:06:33 +0000

    Add new file named size that prints size of data type

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -5,15 +5,11 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-
 printf("Size of a char: %zu  byte(s)\n",sizeof(char));
 printf("Size of a char: %zu  byte(s)\n",sizeof(int));
 printf("Size of a char: %zu  byte(s)\n",sizeof(long int));
 printf("Size of a char: %zu  byte(s)\n",sizeof(float));
-
 return (0);
-
 }

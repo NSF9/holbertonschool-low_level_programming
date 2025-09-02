526bf61beeb97e5fd468f12c43906b41e97b6df4
Nawaf <Alzhraniii999@gmail.com>
2025-09-02 15:57:28 +0000

    print numbers from 1 to 9

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -2,18 +2,17 @@
 /**
  *  print_numbers: prints number from 1 to 9
  *
- *  Return: void 
+ *  Return: void
  */
 void  print_numbers(void)
 {
 
 char c;
 
-for(c = '0'; c  <= '9'; c++)
+for (c = '0'; c <= '9'; c++)
 {
 
 	_putchar(c);
-}	
-
-       _putchar('\n');
+}
+	_putchar('\n');
 }

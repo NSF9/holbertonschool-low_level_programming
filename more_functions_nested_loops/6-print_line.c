4ff016e834f208cae2371923eb2b4eb003f4f241
Nawafls <Alzhraniii999@gmail.com>
2025-09-03 17:09:31 +0300

    print many of lines

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+*print_line - Print _ as you want
+*
+* Return: void
+*/
+void print_line(int n)
+{
+
+int i; 
+
+if (n > 0)
+{
+
+for(i = 0; i < n; i++)
+{
+
+_putchar('_');
+
+}
+
+_putchar('\n');
+}
+
+
+
+}

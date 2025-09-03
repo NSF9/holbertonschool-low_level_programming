36498db75181e122c130a4da8b9d12630ba6d9fa
Nawafls <Alzhraniii999@gmail.com>
2025-09-03 23:45:48 +0300

    print many of lines

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,27 +1,21 @@
 #include "main.h"
+
 /**
-*print_line - Print _ as you want
-*
-* Return: void
-*/
+ * print_line - draws a straight line in the terminal
+ * @n: number of times the character '_' should be printed
+ *
+ * Return: void
+ */
 void print_line(int n)
 {
+	int i;
 
-int i; 
-
-if (n > 0)
-{
-
-for(i = 0; i < n; i++)
-{
-
-_putchar('_');
-
-}
-
-_putchar('\n');
-}
-
-
-
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
 }

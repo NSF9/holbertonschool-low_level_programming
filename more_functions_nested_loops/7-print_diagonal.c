19f2b45fcea66b01cfcd04d9cc42f661d86ce0b8
Nawafls <Alzhraniii999@gmail.com>
2025-09-04 03:24:07 +0300

    Draws a diganols

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
-*print Digonal - draws / line by line
-*
-* Return: void
-*/ 
+ * print_diagonal - draws a diagonal line on the terminal
+ * @n: number of times the character '\' should be printed
+ *
+ * Return: void
+ */ 
 void print_diagonal (int n)
 {
 

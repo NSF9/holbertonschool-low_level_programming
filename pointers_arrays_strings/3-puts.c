ebb53d416252617d01223fcaddeb03b026250dce
Nawafls <Alzhraniii999@gmail.com>
2025-09-05 16:59:28 +0300

    prints the sentence

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string followed by a new line
+ * @str: string to print
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+
+while (*str)
+{
+_putchar(*str);
+
+str++;
+
+}
+_putchar('\n');
+
+
+}

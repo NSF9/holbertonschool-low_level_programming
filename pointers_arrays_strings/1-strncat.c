7f8adab3e54fe8bc2bbd4151e7a6f1e0f88a0713
Nawafls <Alzhraniii999@gmail.com>
2025-09-06 18:54:04 +0300

    copy the number of char

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -12,11 +12,11 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int d_len = 0, s_index = 0;
 
-	/* Find the current length of dest */
+
 	while (dest[d_len])
 		d_len++;
 
-	/* Copy characters from src to dest */
+	
 	while (s_index < n && src[s_index])
 	{
 		dest[d_len] = src[s_index];
@@ -24,7 +24,7 @@ char *_strncat(char *dest, char *src, int n)
 		s_index++;
 	}
 
-	dest[d_len] = '\0'; /* null-terminate */
+	dest[d_len] = '\0';
 
 	return (dest);
 }

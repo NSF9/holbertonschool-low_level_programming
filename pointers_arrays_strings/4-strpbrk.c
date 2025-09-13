6a33a1a438dfda4ab0174afb7b51c42a24e31380
Nawaf <Alzhraniii999@gmail.com>
2025-09-13 16:51:11 +0000

    mathcing words

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: input string to search within
+ * @accept: string containing the set of characters to look for
+ *
+ * Return: pointer to the first occurrence in s of any character from accept,
+ * or NULL if no character is found
+ */
+char *_strpbrk(char *str, char *accept)
+{
+	int i;
+
+	while (*str)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*str == accept[i])
+				return (str);
+		}
+		str++;
+	}
+	return (NULL);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -48,4 +48,7 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 
 unsigned int _strspn(char *s, char *accept);
+
+char *_strpbrk(char *s, char *accept);
+
 #endif

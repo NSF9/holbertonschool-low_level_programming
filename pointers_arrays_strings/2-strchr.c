25ec6a41d671c4a583f6b6c2e0a562d71642aead
Nawaf <Alzhraniii999@gmail.com>
2025-09-13 16:06:08 +0000

    Add stchar to locates a letter

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @Str: string to search in
+ * @Char: character to locate
+ *
+ * Return: pointer to first occurrence of c in s, or NULL
+ */
+char *_strchr(char *Str, char Char)
+{
+	while (*Str != '\0')
+	{
+		if (*Str == Char)
+			return (Str);
+		Str++;
+	}
+
+
+	if (Char == '\0')
+		return (Str);
+
+	return (NULL);
+}

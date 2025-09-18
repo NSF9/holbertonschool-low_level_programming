32539623c0351a38bb015c62af59dbc40a974fbf
Nawaf <Alzhraniii999@gmail.com>
2025-09-18 14:54:42 +0000

    Fill info about my dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -18,11 +18,11 @@ struct dog
 	char *owner;
 };
 
-typedef struct dog dog_st;
+typedef struct dog dog_t;
 
 void init_dog(struct dog *MyDog, char *name, float age, char *owner);
 
 void print_dog(struct dog *d);
 
-dog_st *new_dog(char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner);
 #endif

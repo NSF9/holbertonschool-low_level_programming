2f213e879964b0313adb548ece5a7a2da96927dc
Nawaf <Alzhraniii999@gmail.com>
2025-09-22 13:49:37 +0000

    prints Name by pointer to function and print Upper case

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,4 +1,12 @@
-void print_name(char* name, void (*f)(char*))
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * print_name - print a name
+ * @name: name of the person
+ * @f: Points to function
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
 {
 	f(name);
 }

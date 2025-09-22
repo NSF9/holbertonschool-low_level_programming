da8293f599a9d252549117bcbdc0a1221488d6ba
Nawaf <Alzhraniii999@gmail.com>
2025-09-22 14:30:23 +0000

    prints Name by pointer to function and print Upper case

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -4,11 +4,9 @@
 /**
  * print_name - Prints a name using a callback function.
  * @name: Pointer to a string containing the name.
- * @f: Pointer to a function that takes a char * and returns void.
+ * @f: Function pointer that takes a char * and returns void.
  *
- * Description: This function calls the function pointed to by @f
- * and passes it the @name string to handle the actual printing.
- * Return: void.
+ * Return: Nothing
  */
 void print_name(char *name, void (*f)(char *))
 {

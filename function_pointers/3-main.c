4457de915503461e03933d8c7618e5420c362463
Nawaf <Alzhraniii999@gmail.com>
2025-09-25 10:32:28 +0000

    i created some of function thats calculates numbers

---
+++ function_pointers/3-main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "calc.h"
+
+int main(int argc,char *argv[])
+{
+    int num1, num2;
+
+    char* Op;
+
+    num1 = atoi(argv[1]);
+    num2 = atoi(argv[3]);
+    Op = argv[2];
+
+    if (argc != 4)
+    {
+        printf("Error\n");
+        exit(98);
+    }
+
+    if (get_op_func(Op) == NULL)
+    {
+        printf("Error\n");
+        exit(99);
+    }
+
+    if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
+    {
+        printf("Error\n");
+        exit(100);
+    }
+
+    printf("Result : %d\n", get_op_func(Op)(num1, num2));
+
+    return (0);
+}

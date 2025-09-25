f27a8a1ae06e6b68ccd697daa98c70717fdcc18e
Nawaf <Alzhraniii999@gmail.com>
2025-09-25 11:11:39 +0000

    i created some of function thats calculates numbers

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -4,33 +4,34 @@
 
 int main(int argc,char *argv[])
 {
-    int num1, num2;
+     int num1, num2;
 
-    char* Op;
+ char* Op;
 
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[3]);
-    Op = argv[2];
 
-    if (argc != 4)
-    {
-        printf("Error\n");
-        exit(98);
-    }
+ if (argc != 4)
+ {
+     printf("Error\n");
+     exit(98);
+ }
 
-    if (get_op_func(Op) == NULL)
-    {
-        printf("Error\n");
-        exit(99);
-    }
+ num1 = atoi(argv[1]);
+ num2 = atoi(argv[3]);
+ Op = argv[2];
 
-    if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
-    {
-        printf("Error\n");
-        exit(100);
-    }
+ if (get_op_func(Op) == NULL)
+ {
+     printf("Error\n");
+     exit(99);
+ }
 
-    printf("%d\n", get_op_func(Op)(num1, num2));
+ if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
+ {
+     printf("Error\n");
+     exit(100);
+ }
 
-    return (0);
+ printf("%d\n", get_op_func(Op)(num1, num2));
+
+ return (0);
 }

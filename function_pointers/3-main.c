cec24227f5fdbbdf321a55e54d9044f8e7f31c64
Nawaf <Alzhraniii999@gmail.com>
2025-09-26 07:42:21 +0000

    i created some of function thats calculates numbers

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -19,7 +19,7 @@ int main(int argc,char *argv[])
  num2 = atoi(argv[3]);
  Op = argv[2];
 
- if (get_op_func(Op) == NULL)
+ if (get_op_func(Op) == NULL||Op[1] != '\0')
  {
      printf("Error\n");
      exit(99);

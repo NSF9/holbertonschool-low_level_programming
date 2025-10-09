d7d63d2e9c2e944dfb597709d95cd8cd7f13ebb7
Nawaf <Alzhraniii999@gmail.com>
2025-10-09 12:49:00 +0000

    Handel betty style

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -8,12 +8,12 @@
  *
  * Return: The number of elements in h.
  */
-size_t list_len(const list_t* h)
+size_t list_len(const list_t *h)
 {
 	size_t count;
 
 	for (count = 0; h != NULL; h = h->next)
 		count++;
 
-	return count;
+	return (count);
 }

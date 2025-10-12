9db9b98af3833f42aae6ba540339ffc5b99b5a82
Nawaf <Alzhraniii999@gmail.com>
2025-10-12 13:46:16 +0000

    add New function that prints Reverse nodes

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * add_dnodeint_end - Adds a new node at the end of a doubly linked list
+ * @head: Double pointer to the head of the list
+ * @n: Value to store in the new node
+ *
+ * Return: Address of the new node, or NULL if it failed
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node, *last;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+	return (NULL);
+
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+	new_node->prev = NULL;
+	*head = new_node;
+	return (new_node);
+	}
+
+	last = *head;
+	while (last->next)
+	last = last->next;
+
+	last->next = new_node;
+	new_node->prev = last;
+
+	return (new_node);
+}

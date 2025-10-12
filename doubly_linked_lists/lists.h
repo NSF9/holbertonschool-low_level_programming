67ca8d00669fb9dbcbf6bec842a4922dad62e7af
Nawaf <Alzhraniii999@gmail.com>
2025-10-12 12:35:56 +0000

    add new function

---
+++ doubly_linked_lists/lists.h
@@ -0,0 +1,53 @@
+#ifndef lists_H
+#define lists_H
+#include <stddef.h>
+
+/**
+ * struct dlistint_s - Doubly linked list node structure
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous node in the list
+ * @next: Pointer to the next node in the list
+ *
+ * Description: Node structure for a doubly linked list
+ */
+
+typedef struct dlistint_s
+{
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
+} dlistint_t;
+
+size_t print_dlistint(const dlistint_t *h);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
